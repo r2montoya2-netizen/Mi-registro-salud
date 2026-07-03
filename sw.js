f104993db0930a7e3cc00273a9e98ad65a717e77
r2montoya2-netizen <r2montoya2@gmail.com>
2026-07-04 00:26:18 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'mi-registro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js',
+  'https://fonts.googleapis.com/css2?family=Baloo+2:wght@500;600;700;800&family=Nunito:wght@400;600;700;800&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Estrategia: caché primero, red como respaldo (la app funciona sin conexión)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).then(res => {
+        // guarda en caché las fuentes/recursos que se pidan sobre la marcha
+        if (e.request.url.startsWith('http') && res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
